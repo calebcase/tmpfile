e4179aa5e77e9143ad6beca2a7c2f59951214b26
ccase <caleb@storj.io>
2019-12-21 05:41:53 -0500

    Clarify usage wrt dir.

--- doc_test.go
+++ doc_test.go
@@ -1,7 +1,19 @@
 package tmpfile
 
-func ExampleNew() {
+func ExampleNew_default() {
+	// Use the system default temp directory (as returned by os.TempDir().
+	// This is equivalent to New(os.TempDir(), "example-").
 	f, err := New("", "example-")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+}
+
+func ExampleNew_dir() {
+	// Use a local directory for the temporary files. This directory must
+	// already exist.
+	f, err := New("ephemeral", "example-")
 	if err != nil {
 		panic(err)
 	}
